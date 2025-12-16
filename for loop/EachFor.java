3e0c1a0b8ea47e31f495fcff7b26c0feaa53256b
svk-vasanthkumar <vasanth636807@gmail.com>
2025-12-16 18:21:18 +0530

    14-12-2025

---
+++ for loop/EachFor.java
@@ -0,0 +1,8 @@
+class EachFor{
+    public static void main(String args[]){
+        float[] arr = {4.3f,3.4f};
+        for(float svkk:arr){
+            System.out.println(svkk);
+        }
+    }
+}
---
+++ functions/keyReturn.java
@@ -0,0 +1,12 @@
+class keyReturn {
+    
+    int svk(){
+            return 100;
+    }
+    public static void main(String args[]){
+        keyReturn vk = new keyReturn();
+        int re=vk.svk();
+        System.out.print(re);
+
+    }
+}
