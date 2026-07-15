e7074f98202d6640ca71b815f9dd581eb885453b
svk-vasanthkumar <vasanth636807@gmail.com>
2026-07-15 07:15:32 +0530

    Practice the scanner

---
+++ PracScan.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+ class PraScan{
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int value = scan.nextInt();
+        System.out.print(value);
+    }
+ }
